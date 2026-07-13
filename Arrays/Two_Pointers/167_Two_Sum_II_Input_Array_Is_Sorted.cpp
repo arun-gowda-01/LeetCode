f3c6f575d0ddc54acc53af1dcd6197a236e98980
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-13 19:51:50 +0530

    Add files via upload

---
+++ Arrays/Two_Pointers/167_Two_Sum_II_Input_Array_Is_Sorted.cpp
@@ -0,0 +1,25 @@
+// LeetCode 167
+// Problem: Two Sum II - Input Array Is Sorted
+// Topic: Two Pointers
+// Difficulty: Medium
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int left = 0, right = numbers.size() - 1;
+
+        while (left < right) {
+            if (numbers[left] + numbers[right] == target)
+                return {left + 1, right + 1};
+
+            if (numbers[left] + numbers[right] < target)
+                left++;
+            else
+                right--;
+        }
+
+        return {-1, -1};
+    }
+};
