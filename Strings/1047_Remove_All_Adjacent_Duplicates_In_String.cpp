401005f1e956356db55540c9419befd455fb379d
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-20 20:33:03 +0530

    Strings

---
+++ Strings/1047_Remove_All_Adjacent_Duplicates_In_String.cpp
@@ -0,0 +1,19 @@
+// LeetCode 1047
+// Remove All Adjacent Duplicates In String
+
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        string result;
+
+        for (int i = 0; i < s.size(); i++) {
+            if (!result.empty() && result.back() == s[i]) {
+                result.pop_back();
+            } else {
+                result.push_back(s[i]);
+            }
+        }
+
+        return result;
+    }
+};
