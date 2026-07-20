93476f9b7a0acd8cf51528dc0f93e46298ef6095
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-20 20:29:55 +0530

    Strings

---
+++ Strings/647_Palindromic_Substrings.cpp
@@ -0,0 +1,33 @@
+// LeetCode 647
+// Palindromic Substrings
+
+class Solution {
+public:
+    int countSubstrings(string s) {
+        int n = s.size();
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            // Odd-length palindromes
+            int left = i, right = i;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                count++;
+                left--;
+                right++;
+            }
+
+            // Even-length palindromes
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < n && s[left] == s[right]) {
+                count++;
+                left--;
+                right++;
+            }
+        }
+
+        return count;
+    }
+};
