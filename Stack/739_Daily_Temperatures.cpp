718b0b5fcac4b54526d00af4a42b26a3feec85ec
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-21 19:30:23 +0530

    Monotonic Stack

---
+++ Stack/739_Daily_Temperatures.cpp
@@ -0,0 +1,22 @@
+// LeetCode 739
+// Daily Temperatures
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        int n = temperatures.size();
+        vector<int> ans(n, 0);
+        stack<int> st;
+
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && temperatures[st.top()] < temperatures[i]) {
+                ans[st.top()] = i - st.top();
+                st.pop();
+            }
+
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
