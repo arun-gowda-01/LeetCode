afe2576d069c69faa4bc6f502557412adb79ec79
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-28 20:14:46 +0530

    Linked List

---
+++ Linked List/160_Intersection_of_Two_Linked_Lists.cpp
@@ -0,0 +1,33 @@
+// LeetCode 160
+// Intersection of Two Linked Lists
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ * };
+ */
+
+class Solution {
+public:
+    ListNode* getIntersectionNode(ListNode* headA, ListNode* headB) {
+        ListNode* pA = headA;
+        ListNode* pB = headB;
+
+        while (pA != pB) {
+            if (!pA)
+                pA = headB;
+            else
+                pA = pA->next;
+
+            if (!pB)
+                pB = headA;
+            else
+                pB = pB->next;
+        }
+
+        return pA;
+    }
+};
