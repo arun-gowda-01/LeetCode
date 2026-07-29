471eb0b3a3374638f864b21fdf11b180d76c6178
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-29 21:28:26 +0530

    Recursion + DP

---
+++ Recursion and Backtracking/70_Climbing_Stairs.cpp
@@ -0,0 +1,23 @@
+// LeetCode 70
+// Climbing Stairs
+
+class Solution {
+public:
+    vector<int> dp;
+
+    int solve(int n) {
+        if (n == 1 || n == 2)
+            return n;
+
+        if (dp[n] != -1)
+            return dp[n];
+
+        return dp[n] = solve(n - 1) + solve(n - 2);
+    }
+
+    int climbStairs(int n) {
+        dp.resize(n + 1, -1);
+
+        return solve(n);
+    }
+};
