2f1a3a8044aeeea3e96a0830ceea7e6220670dc8
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-30 19:38:36 +0530

    Recursion

---
+++ Recursion and Backtracking/344_Reverse_String.cpp
@@ -0,0 +1,18 @@
+// LeetCode 344
+// Reverse String
+
+class Solution {
+public:
+    void solve(vector<char>& s, int left, int right) {
+        if (left >= right)
+            return;
+
+        swap(s[left], s[right]);
+
+        solve(s, left + 1, right - 1);
+    }
+
+    void reverseString(vector<char>& s) {
+        solve(s, 0, s.size() - 1);
+    }
+};
