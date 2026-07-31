2e2d221ba816a9bd3c5ce4056a462a6628b4fcdd
Arun Kumar N S <arunkumarns806@gmail.com>
2026-07-31 19:16:03 +0530

    Recursion

---
+++ Recursion and Backtracking/125_Valid_Palindrome.cpp
@@ -0,0 +1,25 @@
+// LeetCode 125
+// Valid Palindrome - Recursive Approach
+
+class Solution {
+public:
+    bool solve(string& s, int left, int right) {
+        if (left >= right)
+            return true;
+
+        if (!isalnum(s[left]))
+            return solve(s, left + 1, right);
+
+        if (!isalnum(s[right]))
+            return solve(s, left, right - 1);
+
+        if (tolower(s[left]) != tolower(s[right]))
+            return false;
+
+        return solve(s, left + 1, right - 1);
+    }
+
+    bool isPalindrome(string s) {
+        return solve(s, 0, s.size() - 1);
+    }
+};
