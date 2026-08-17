f7a983bf8da1d4615126c674008ec24568636659
Arun Kumar N S <arunkumarns806@gmail.com>
2026-08-17 21:58:40 +0530

    Constraint Backtracking

---
+++ Recursion and Backtracking/51_N_Queens.cpp
@@ -0,0 +1,51 @@
+// LeetCode 51
+// N-Queens
+
+class Solution {
+public:
+    bool isValid(int row, int col, vector<string>& board, int n) {
+        for (int i = row - 1; i >= 0; i--) {
+            if (board[i][col] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        for (int i = row - 1, j = col + 1; i >= 0 && j < n; i--, j++) {
+            if (board[i][j] == 'Q')
+                return false;
+        }
+
+        return true;
+    }
+
+    void solve(int row, int n, vector<string>& board,
+               vector<vector<string>>& ans) {
+        if (row == n) {
+            ans.push_back(board);
+            return;
+        }
+
+        for (int col = 0; col < n; col++) {
+            if (isValid(row, col, board, n)) {
+                board[row][col] = 'Q';
+
+                solve(row + 1, n, board, ans);
+
+                board[row][col] = '.';
+            }
+        }
+    }
+
+    vector<vector<string>> solveNQueens(int n) {
+        vector<vector<string>> ans;
+        vector<string> board(n, string(n, '.'));
+
+        solve(0, n, board, ans);
+
+        return ans;
+    }
+};
