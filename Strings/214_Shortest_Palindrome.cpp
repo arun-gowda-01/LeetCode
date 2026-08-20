6ba02a886991828e9b38076710e81b04d5010dc8
Arun Kumar N S <arunkumarns806@gmail.com>
2026-08-20 20:00:28 +0530

    String Matching

---
+++ Strings/214_Shortest_Palindrome.cpp
@@ -0,0 +1,33 @@
+// LeetCode 214
+// Shortest Palindrome
+
+class Solution {
+public:
+    string shortestPalindrome(string s) {
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+
+        string temp = s + "#" + rev;
+
+        vector<int> lps(temp.size(), 0);
+
+        for (int i = 1; i < temp.size(); i++) {
+            int j = lps[i - 1];
+
+            while (j > 0 && temp[i] != temp[j])
+                j = lps[j - 1];
+
+            if (temp[i] == temp[j])
+                j++;
+
+            lps[i] = j;
+        }
+
+        int len = lps.back();
+
+        string remaining = s.substr(len);
+        reverse(remaining.begin(), remaining.end());
+
+        return remaining + s;
+    }
+};
