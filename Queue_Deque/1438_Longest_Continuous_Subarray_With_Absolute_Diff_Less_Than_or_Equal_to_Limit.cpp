3a858462ab41875449173003e8b3893cf19aaad0
Arun Kumar N S <arunkumarns806@gmail.com>
2026-08-28 20:21:54 +0530

    Deque

---
+++ Queue_Deque/1438_Longest_Continuous_Subarray_With_Absolute_Diff_Less_Than_or_Equal_to_Limit.cpp
@@ -0,0 +1,37 @@
+// LeetCode 1438
+// Longest Continuous Subarray With Absolute Diff Less Than or Equal to Limit
+
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums, int limit) {
+        deque<int> maxdq;
+        deque<int> mindq;
+        int ans = 0, left = 0;
+
+        for (int right = 0; right < nums.size(); right++) {
+            while (!maxdq.empty() && nums[maxdq.back()] <= nums[right])
+                maxdq.pop_back();
+
+            maxdq.push_back(right);
+
+            while (!mindq.empty() && nums[mindq.back()] >= nums[right])
+                mindq.pop_back();
+
+            mindq.push_back(right);
+
+            while (nums[maxdq.front()] - nums[mindq.front()] > limit) {
+                if (maxdq.front() == left)
+                    maxdq.pop_front();
+
+                if (mindq.front() == left)
+                    mindq.pop_front();
+
+                left++;
+            }
+
+            ans = max(ans, right - left + 1);
+        }
+
+        return ans;
+    }
+};
