a8754450c46090fdad7b14412984db5996b9fe07
Arun Kumar N S <arunkumarns806@gmail.com>
2026-08-31 19:47:45 +0530

    DP + Deque

---
+++ Queue_Deque/1696_Jump_Game_VI.cpp
@@ -0,0 +1,28 @@
+// LeetCode 1696
+// Jump Game VI
+
+class Solution {
+public:
+    int maxResult(vector<int>& nums, int k) {
+        int n = nums.size();
+        vector<int> dp(n);
+        deque<int> dq;
+
+        dp[0] = nums[0];
+        dq.push_back(0);
+
+        for (int i = 1; i < n; i++) {
+            while (!dq.empty() && dq.front() < i - k)
+                dq.pop_front();
+
+            dp[i] = nums[i] + dp[dq.front()];
+
+            while (!dq.empty() && dp[i] >= dp[dq.back()])
+                dq.pop_back();
+
+            dq.push_back(i);
+        }
+
+        return dp[n - 1];
+    }
+};
